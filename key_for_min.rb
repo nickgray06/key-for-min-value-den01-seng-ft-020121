0219a82de12a3b6c57c58d44ed2ff76f654c1adb
Nick Gray <nickgray87@gmail.com>
2021-01-21 21:08:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,11 @@
 def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each do |key, value|
-    if value.min
+    if value > min_value
       min_value = value
+      if min_value > value
+        min_value = value
+      end
     end
     puts key
   end
